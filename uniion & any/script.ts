4f7e56b1fbfb4a583b97c6d73d1330368c031c3d
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-24 23:26:28 +0530

    added union & any

---
+++ uniion & any/script.ts
@@ -0,0 +1,12 @@
+// union and any
+
+
+// union
+let subs:number | string= '10' // now it can accept 2 types of values number and union.
+
+// it is user 
+
+let apireq : 'pending' | 'success' | 'error' = 'pending'
+
+apireq :'done'
+
