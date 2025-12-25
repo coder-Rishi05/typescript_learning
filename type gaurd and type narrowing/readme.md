d0f915109668bb4c0cc3790281c776d4f678e949
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-25 18:11:59 +0530

    added type gaurd

--- type gaurd and type narrowing/readme.md
+++ type gaurd and type narrowing/readme.md
@@ -1,7 +1,11 @@
 The `unknown type` 
 
 
-jhan jhan any likhte han vha unknown likh skte han,
+jhan jhan `any` likhte han vha unknown likh skte han,
 it is safe then any.
 
+unknown work same as just that we tell it before we use.
+
+
 type narrowing
+
--- type gaurd and type narrowing/script.ts
+++ type gaurd and type narrowing/script.ts
@@ -1,8 +1,45 @@
 function getItem (kind:string | number){
     if(typeof kind === 'string'){
-        return `this is ${kind} : number`
+        return `this is ${kind} : string`
     }
-    return `this is ${kind} : string`
+    return `this is ${kind} : number`
+
+
+
 }
 
-// now yhan pe types badal jaaenge
+function setItem(msg ?: string){
+    if(msg){
+        return `getting ${msg}`
+    }
+    return `setting deafult message ${msg} `
+}
+
+// now yhan pe types badal jaaenge on value to now basically we will get specific methods related to types in suggetions.
+
+// 
+
+function order(size: "small" | "medium" | 'large' | number ){
+    if (size === 'medium') {
+        return "medium"
+    }else
+        return "large"
+}
+
+class kill {
+    server(){
+
+    }
+}
+class cut {
+    server(){
+
+    }
+}
+
+// creating user defined types
+
+type chaiOrder = {
+    type:string,
+    sugar:number
+}
