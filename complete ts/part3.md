554d24b20b9212ccf7288a36429634b6b098a922
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-05 11:30:15 +0530

    started classes

---
+++ complete ts/part3.md
@@ -0,0 +1,41 @@
+# classes and objects (oops)
+
+- topic
+
+-  •Class definition
+
+
+-  Classes and Objects
+
+it is basically structure.
+
+```js
+
+class Device {
+    name="lg";
+    price=12000
+    category = "digital"
+}
+
+let device_One = new Device()
+
+
+
+```
+
+
+-  •Constructors
+
+-  •Access modifiers (public, private, protected)
+
+-  •Readonly properties
+
+-  •Optional properties
+
+-  •Parameter properties
+
+-  •Getters and setters
+
+-  •Static members
+
+-  •Abstract classes and methods
--- complete ts/test.js
+++ complete ts/test.js
@@ -1,9 +1,21 @@
-// type alias
-var a;
-// now a's type will automatically be number
-a = 45;
-var b;
-b = 1;
-b = "this is string";
-b = null;
-// now b can have 3 types or ab ek hi varible me ye likhne ki jrurat nhi hai  number | string | null basically shorthand ki tarah kaam krega
+// Classes and Objects
+// •Class definition
+// •Constructors
+// •Access modifiers (public, private, protected)
+// •Readonly properties
+// •Optional properties
+// •Parameter properties
+// •Getters and setters
+// •Static members
+// •Abstract classes and methods
+var Device = /** @class */ (function () {
+    function Device() {
+        this.name = "lg";
+        this.price = 12000;
+        this.category = "digital";
+    }
+    return Device;
+}());
+var device_One = new Device();
+
+var device_two = new Device();
