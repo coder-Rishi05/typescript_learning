602e6c05ad9a61184745cb2164755f70a93a4947
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-16 23:20:15 +0530

    added types

---
+++ typescript pj-2 (Typed CLI Expense Tracker)/Typed CLI Expense Tracker/src/types.ts
@@ -0,0 +1,29 @@
+export enum Category {
+  FOOD = "food",
+  TRAVEL = "travel",
+  SHOPPING = "shopping",
+  OTHER = "other",
+}
+
+export interface expence {
+  readonly id: number;
+  title: string;
+  amount: number;
+  category: Category;
+  createdAt: Date;
+}
+export type Command =
+  | { type: "add"; title: string; amount: number }
+  | { type: "list" }
+  | { type: "total" }
+  | { type: "delete"; id: number };
+
+export type Result<T> =
+  | { success: true; data: T }
+  | { success: false; error: string };
+
+export type NewExpenseInput = {
+  title: string;
+  amount: number;
+  category?: Category;
+};
